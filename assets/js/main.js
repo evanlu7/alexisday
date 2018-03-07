a145dce7d0c1949e898c70414ec226e2f4ce6a98
Evan Lu <evanlu7@gmail.com>
2018-03-06 16:17:44 -0800

    adding middle section

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,45 +1,57 @@
-$(document).ready(function(){       
-            var scroll_pos = 0;
-            $(document).scroll(function() { 
-                scroll_pos = $(this).scrollTop();
-                if(scroll_pos > 50) {
-                    $("nav").css('background-color', 'white');
-                    $("nav a").css('color', 'black');
-                } else {
-                    $("nav").css('background-color', 'transparent');
-                    $("nav a").css('color', 'white');
-                }
-            });
+$(document).ready(function() {
+  var scroll_pos = 0;
+  $(document).scroll(function() {
+    scroll_pos = $(this).scrollTop();
+    if (scroll_pos > 50) {
+      $("nav").css('background-color', 'white');
+      $("nav a").css('color', 'black');
+    } else {
+      $("nav").css('background-color', 'transparent');
+      $("nav a").css('color', 'white');
+    }
+  });
 });
 
-$(document).ready(function(){       
-            var scroll_pos = 0;
-            $(document).scroll(function() { 
-                scroll_pos = $(this).scrollTop();
-                if(scroll_pos > 90) {
-                    $(".social").addClass("animated bounceIn");
-                } else {
-                  $(".social").addClass("");
-                }
-            });
-
-$(document).ready(function(){       
-            var scroll_pos = 0;
-            $(document).scroll(function() { 
-                scroll_pos = $(this).scrollTop();
-                
-                if(scroll_pos > 50) {
-                    $("button").css('background-color', '');
-                    $("button").addClass('animated flip');
-                    $("button").addClass('visibility: visible');
-            
-                } else {
-                    $("button").css('background-color', '');
-                    $("button").addClass('');
-                }
-            });
-        });
-            
+$(document).ready(function() {
+  var scroll_pos = 0;
+  $(document).scroll(function() {
+    scroll_pos = $(this).scrollTop();
+    if (scroll_pos > 90) {
+      $(".social").addClass("animated bounceIn");
+    } else {
+      $(".social").addClass("");
+    }
+  });
 });
 
+$(document).ready(function() {
+  var scroll_pos = 0;
+  $(".a1").addClass('visibility: hidden');
+  $(document).scroll(function() {
+    scroll_pos = $(this).scrollTop();
+    if (scroll_pos > 800) {
+      $(".a1").addClass("animated slideInRight");
+       $(".a1").addClass('visibility: visible');
+    } else {
+        $(".a1").addClass('visibility: hidden');
+    }
+  });
+});
+
+$(document).ready(function() {
+var scroll_pos = 0;
+$(document).scroll(function() {
+  scroll_pos = $(this).scrollTop();
+
+  if (scroll_pos > 50) {
+    $("button").css('background-color', '');
+    $("button").addClass('animated flip');
+    $("button").addClass('visibility: visible');
+
+  } else {
+    $("button").css('background-color', '');
+    $("button").addClass('');
+  }
+    });
+});
 
